62cfaac46106730200374f5ac3038857daff1726
Catter <1561495202@qq.com>
2016-04-13 13:22:18 +0800

    change

--- application/views/newsEditor.php
+++ application/views/newsEditor.php
@@ -6,9 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <title>newsEditor</title>
-    <link rel="stylesheet" type="text/css" href="./dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./dist/css/wangEditor.min.css">
-    <link rel="stylesheet" type="text/css" href="./dist/css/EditorStyle.css">
+    <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="dist/css/wangEditor.min.css">
+    <link rel="stylesheet" type="text/css" href="dist/css/EditorStyle.css">
     <style>
         .input-box span,input{
             font-family:  微软雅黑,sans-serif;
@@ -27,7 +27,46 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="filter-bg"></div>
         <div class="main-title" style="font-family: 微软雅黑; color: #6f6e6b;">专为新闻编辑设计</div>
         <div class="btn-R-position">
-            <button class="btn btn-default">登录</button>
+            <button type="button" class="btn btn-default" data-toggle="modal" data-target="#logIn">登录</button>
+        </div>
+        <div class="modal fade" id="logIn" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">登录</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">用户名</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="inputEmail3" placeholder="username">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword3" class="col-sm-2 control-label">密码</label>
+                                <div class="col-sm-8">
+                                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> Remember me
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                        <button type="button" class="btn btn-primary" >登录</button>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="select-box">
             <span class="select-title">类别选择</span>
@@ -48,15 +87,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <div class="input-box">
             <span>发布单位</span>
-            <input type="text" placeholder="这是单位">
+            <input type="text" placeholder="company">
         </div>
         <div class="input-box">
             <span>新闻标题</span>
-            <input type="text" placeholder="这是标题">
+            <input type="text" placeholder="title">
         </div>
         <div class="input-box">
             <span>编辑作者</span>
-            <input type="text" placeholder="这是作者">
+            <input type="text" placeholder="author">
         </div>
         <div class="input-box">
             <span>新闻内容</span>
@@ -67,27 +106,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>
         </div>
         <div class="input-box">
-            <button class="btn btn-default btn-R-position">发布</button>
+            <button type="button" id="showView" class="btn btn-default">查看效果</button>
+            <button type="button" id="release" class="btn btn-default btn-R-position">发布</button>
         </div>
     </div>
 
-    <script type="text/javascript" src="./dist/js/lib/jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="./dist/js/wangEditor.js"></script>
-    <script type="text/javascript" src="./dist/js/lib/bootstrap.min.js"></script>
+    <script type="text/javascript" src="dist/js/lib/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="dist/js/wangEditor.js"></script>
+    <script type="text/javascript" src="dist/js/lib/bootstrap.min.js"></script>
     <script>
         //页面简单逻辑
         (function($){
             //选择框的逻辑
-            var selected = $("#selected-category");
-            var selected_item = selected.find("button");
-            selected.find("ul").children().each(function(){
-                $(this).bind("click", function(event){
-                    event.preventDefault();
-                    var item = $(this).text();
-                    selected_item.text(item);
-                    selected_item.append('<span class="caret"></span>');
-                });
-            });
+            var selectBox = function(){};
+            selectBox.prototype = {
+                selectCategory : function(){
+                    var selected = $("#selected-category");
+                    var selected_item = selected.find("button");
+                    selected.find("ul").children().each(function(){
+                        $(this).bind("click", function(event){
+                            event.preventDefault();
+                            var item = $(this).text();
+                            selected_item.text(item);
+                            selected_item.append('<span class="caret"></span>');
+                        });
+                    });
+                }
+            };
+            var selectbox = new selectBox();
+            selectbox.selectCategory();
+
+            //登录按钮提交表单
+
+            //查看效果（访问template页面）跳转页面请求
+
+            //发布页面ajax提交，弹出框，refresh
+
         })(jQuery);
 
         //编辑器逻辑
